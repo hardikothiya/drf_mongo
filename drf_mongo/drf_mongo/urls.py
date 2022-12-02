6f0422546c9412a641a014fd2db4ea568eb55cf5
hardik kothiya <hardik.kothiya@chainsense.com>
2022-12-02 14:10:55 +0530

    Mongo Added

--- drf_mongo/drf_mongo/urls.py
+++ drf_mongo/drf_mongo/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url, include
+from django.conf.urls import include
 
+from django.urls import re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
