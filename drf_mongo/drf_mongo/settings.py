73c77515f9c9e01ec87398e16fc925a66e69b9ad
hardik kothiya <hardik.kothiya@chainsense.com>
2022-12-02 14:48:27 +0530

    Revert "Revert "Mongo Added""
    This reverts commit 7bafcf2379dac81cb4267a92cdc3f5bbe4029b1d.

--- drf_mongo/drf_mongo/settings.py
+++ drf_mongo/drf_mongo/settings.py
@@ -92,6 +92,18 @@ DATABASES = {
     }
 }
 
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': "testdb",
+#        'USER': 'postgres',
+#        'PASSWORD': 'hardik991322',
+#        'HOST': '127.0.0.1',
+#        'PORT': 5432,
+#    }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -198,13 +210,13 @@ LOGGING = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
